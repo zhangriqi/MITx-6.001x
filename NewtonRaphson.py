bb7c1810e5b545d778ce07bbbedbc50713eec47a
zhangriqi <zzhangruiqi719@gmail.com>
2018-05-03 16:49:37 +0800

    Create NewtonRaphson.py

---
+++ NewtonRaphson.py
@@ -0,0 +1,12 @@
+# Use g-p(g)/p'(g) to quickly find the solution to any equation
+
+epsilon = 0.01
+y = 24.0
+guess = y/2.0
+numGuesses = 0
+
+while abs(guess*guess - y) >= epsilon:
+    numGuesses += 1
+    guess = guess - (guess**2-y)/(2*guess)
+print("numGuesses = " + str(numGuesses))
+print("Square root of " + str(y) + " is about " + str(guess)) 
